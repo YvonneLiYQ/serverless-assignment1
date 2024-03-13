1ce2a95ddc0b884df3ce7f614a30cf5421421949
YangQing Li <2217175013@qq.com>
2024-03-13 18:47:52 +0000

    Confirm signup resource.

--- lib/auth-app-stack.ts
+++ lib/auth-app-stack.ts
@@ -46,6 +46,12 @@ export class AuthAppStack extends cdk.Stack {
       "SignupFn",
       'signup.ts'
     );
+    this.addAuthRoute(
+      "confirm_signup",
+      "POST",
+      "ConfirmFn",
+      "confirm-signup.ts"
+    );
 
   
   }
